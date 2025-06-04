17bcbe4ecd150d0b8571f4785c3b2fdad3024562
dkii9300 <dmitry0926@gmail.com>
2025-06-04 01:45:13 +0100

    Create formulas.py

---
+++ modules/formulas.py
@@ -0,0 +1,5 @@
+FORMULAS = {
+    "CLO_MCR": r"CLO = \\frac{MCR}{1000} \\times \\text{CLO}_{cy} \\times Z",
+    "SFOC": r"SFOC = \\frac{\\text{Fuel mass}}{P \\times t} \\times 10^6",
+    "EEXI": r"EEXI = \\frac{P_{ME} \\times SFOC \\times C_F}{DWT \\times V_{ref}}"
+}
