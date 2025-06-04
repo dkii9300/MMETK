fa89bc20556712edcb13e1245e26a0c5cfd6b917
dkii9300 <dmitry0926@gmail.com>
2025-06-04 01:47:04 +0100

    Create localization.py

---
+++ modules/localization.py
@@ -0,0 +1,14 @@
+TRANSLATIONS = {
+    "en": {
+        "title": "Marine Main Engine Toolkit",
+        "select_engine": "Select engine model",
+        "clo_calc": "Cylinder Oil Consumption",
+        "eexi_calc": "EEXI Calculation"
+    },
+    "ka": {
+        "title": "ზღვაური ძრავის ხელსაწყო",
+        "select_engine": "აირჩიე ძრავის მოდელი",
+        "clo_calc": "ცილინდრული ზეთის ხარჯვა",
+        "eexi_calc": "EEXI გაანგარიშება"
+    }
+}
