c149af78c51542256bbc2c02cb1d2372c0095b20
dkii9300 <dmitry0926@gmail.com>
2025-06-04 01:43:44 +0100

    Create models.py

---
+++ modules/models.py
@@ -0,0 +1,22 @@
+ENGINE_DATA = {
+    "6S50MC": {
+        "cylinders": 6,
+        "bore": 500,
+        "stroke": 2190,
+        "MCR": 9480,
+        "speed": 127,
+        "SFOC": 175,
+        "CLO_per_cyl": 1.3,
+        "BN_recommended": 70
+    },
+    "6S60MC": {
+        "cylinders": 6,
+        "bore": 600,
+        "stroke": 2400,
+        "MCR": 12200,
+        "speed": 105,
+        "SFOC": 171,
+        "CLO_per_cyl": 1.5,
+        "BN_recommended": 70
+    }
+}
